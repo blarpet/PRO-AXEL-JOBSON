937a72144b0bd9d674c615123cabe0354619ac79
blarpet <axejob0505@gmail.com>
2020-09-09 11:39:28 +0200

    9/9 Läxa

---
+++ Variabler/variabler.py
@@ -0,0 +1,12 @@
+name = "Axel" # Har skapat variabel name och tilldelat det värdet Axel.
+age = 29 # Har skapat variabel age och tilldelat det värdent 29.
+
+print(f"Hej {name} du är {age} år gammal.")
+
+sida = float(input("Ange kvadratens sida: "))
+
+area = sida**2
+
+omkrets = 4*sida
+
+print(f"Kvadratens area är {area} a.e. och kvadratens omkrets är {omkrets} l.e.")
