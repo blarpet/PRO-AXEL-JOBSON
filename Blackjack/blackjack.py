d03fed69e3ab90d3a15961189ef7596405149174
blarpe  git config --global user.email you@example.com <you@example.com>
2020-12-14 14:18:46 +0100

    stuff

---
+++ Blackjack/blackjack.py
@@ -0,0 +1,46 @@
+import kortlek
+
+lek = kortlek.skapaKortlek()
+
+print(lek)
+
+# behöver räkna ut kort
+
+# hand
+def skrivUtHanden(hand):
+    print("Dina kort är:", end="")
+    for kort in hand:
+        print(str(kort) + ", ", end="")
+
+# checka vinnaren
+
+# spel-loop
+while True:
+    spela = input("Vill du spela blackjack. (Y/N) ")
+
+    if spela != "Y":
+        break
+
+    lek = kortlek.skapaKortlek()
+
+    print(lek)
+    # Dealer tar 2 kort
+    dealer = [lek.pop(0), lek.pop(0)]
+    print(f"Dealerns första kort är {dealer[0]}")
+
+    hand = [lek.pop(0), lek.pop(0)]
+    print(f"Dina första två kort är: {hand[0]} och {hand[1]}")
+
+    fortsätt = True
+    # göra val (ta ett till kort eller stanna   )  
+    while fortsätt:
+    # fråga användaren om hen vill ta emot ett kort
+    taKort = input("Ta ett till kort [Y/N]")
+    
+    if taKort == "Y":
+        # Dela ut kort
+        hand.append(lek.pop(0))
+        # Skriv ut handen
+        skrivUtHanden(hand)
+    else:
+        fortsätt = False    
---
+++ Blackjack/kortlek.py
@@ -0,0 +1,20 @@
+import random as rnd
+
+def skapaKortlek():
+    kortNummer = [i for i in range(2,11)]
+    klädKort = ["Kn", "D", "K", "A"]
+    kortNummer += klädKort
+    lek = 4*kortNummer
+    blandaKort(lek)
+
+    return lek
+
+# blanda kort
+
+def blandaKort(lek):
+    return rnd.shuffle(lek)
+
+
+kortlek = skapaKortlek()
+
+print(kortlek)
