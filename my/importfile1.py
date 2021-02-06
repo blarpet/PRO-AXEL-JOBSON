5e42965fd6900543c080c3285f5c077ac471c46f
blarpet <70134967+blarpet@users.noreply.github.com>
2021-02-06 21:38:13 +0100

    g

---
+++ my/importfile1.py
@@ -0,0 +1,9 @@
+def omkrets(r):
+    pi = 3.14
+    o = (r+r)*pi
+    return print(f"Omkrets är {o}")
+
+def area(r):
+    pi = 3.14
+    a = r*r*pi
+    return print(f"Area är {a}")
---
+++ my/m05u07/m05u07_funktioner.py
@@ -0,0 +1,12 @@
+
+def julgran(x):
+
+    y = "*"
+
+    for i in range(x):
+        print(y)
+        y += "*"
+
+def tomten():
+    print("Ho, Ho, Ho!")
+    print("Jullovet är nära")
