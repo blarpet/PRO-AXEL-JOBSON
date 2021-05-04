def4dfb982c93396ba84e3cb0f51cd5bc74b358c
blarpe  git config --global user.email you@example.com <you@example.com>
2021-05-04 15:48:39 +0200

    gg

--- COVIDPandasUppgift/code.py
+++ COVIDPandasUppgift/code.py
@@ -8,24 +8,33 @@ nt = pd.read_csv("National_Total_Deaths_by_Age_Group.csv")
 
 
 
-while run = True:
+while run == True:
     q1 = str(input("Do you wish to either view National Daily Deaths or the National Total Deaths by Age Group? [NDD/NTDAG/NEITHER]"))
     if q1 == "NDD":
-        q1.2 = str(input("Do you wish to either view Line chart or a Frequency diagram of the National Daily Deaths? [LC/FD]"))
-        if q1.2 == "LC":
+        q1_2 = str(input("Do you wish to either view Line chart or a Frequency diagram of the National Daily Deaths? [LC/FD]"))
+        if q1_2 == "LC":
             plt.title("National Daily Deaths in result of Covid")
-            plt.plot(nd.date, nd.National_Daily_Deaths "b.-")
-            plt.xticks(nd.date[::30])
+            plt.plot(nd.Date, nd.National_Daily_Deaths, "b.-")
+            plt.xticks(nd.Date[::75])
             plt.xlabel("Day")
             plt.ylabel("Casualties")
             plt.show()
-        elif q1.2 == "FD":
+        elif q1_2 == "FD":
+            bins = [0,10,20,30,40,50,60,70,80,90,100,110,120]
+            plt.title("Frequency of National Daily Deaths in result of Covid")
+            plt.hist(nd.National_Daily_Deaths, bins=bins)
+            plt.xticks(bins)
+            plt.yticks()
+            plt.ylabel("Casualties")
+            plt.show()
             #Loads the frequency diagram of the NDD
-    if q1 == "NTDAG":
-        q1.3 = str(input("Do you wish to either view Pie chart or a Bar chart of the National Daily Deaths? [PC/BC]"))
-        if q1.3 == "PC":
-            #Loads the pie chart of NTDAG
-        if q1.3 == "BC":
+    elif q1 == "NTDAG":
+        q1_3 = str(input("Do you wish to either view Pie chart or a Bar chart of the National Daily Deaths? [PC/BC]"))
+        if q1_3 == "PC":
+            print("g")
+            #Loads the pie chart of NTDAG   
+        if q1_3 == "BC":
+            print("g")
             #Loads the bar chart of NTDAG
     elif q1 == "NEITHER":
         run = False
